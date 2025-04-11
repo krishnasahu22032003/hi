52b833cf9f2d6c2e7b247eb14c15e08a2076b2dd
krishna sahu <krishnasahu22official@gmail.com>
2025-04-11 20:53:33 +0530

    this is git trail

---
+++ http.js
@@ -0,0 +1,21 @@
+const express= require('express')
+// const cors = require('cors')
+const app=express()
+
+app.use(express.json())
+// app.use(cors())
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/public/cors.html')
+})
+app.post('/sum', function(req, res) {
+
+    const a = parseInt(req.body.a);
+    const b = parseInt(req.body.b);
+
+    res.json({
+        ans: a + b
+    })
+});
+
+
+app.listen(8000)
